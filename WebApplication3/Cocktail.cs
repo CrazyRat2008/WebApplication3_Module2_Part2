0665048ce006bdaf6611f132cceaffeab090b663
Jonika <katyadyduch@gmail.com>
2023-03-12 21:24:44 +0200

    Добавьте файлы проекта.

---
+++ WebApplication3/Cocktail.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3
+{
+    public class Cocktail
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Description { get; set; }
+    }
+    public class CocktailController : Controller
+    {
+        private readonly List<Cocktail> cocktails;
+
+        public CocktailController(List<Cocktail> cocktails)
+        {
+            this.cocktails = cocktails;
+        }
+
+        public IActionResult Index()
+        {
+            return View(cocktails);
+        }
+    }
+
+
+
+}
---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<List<Cocktail>>(new List<Cocktail>
+    {
+        new Cocktail { Name = "Margarita", Ingredients = "Tequila, lime juice, triple sec", Description = "A classic tequila cocktail" },
+        new Cocktail { Name = "Manhattan", Ingredients = "Whiskey, sweet vermouth, bitters", Description = "A classic whiskey cocktail" },
+        new Cocktail { Name = "Gin and Tonic", Ingredients = "Gin, tonic water, lime", Description = "A refreshing gin cocktail" }
+    });
+
+            services.AddMvc();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<CocktailController>();
+        }
+    }
+}
